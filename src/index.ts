6d9fd61c38ed7e35fc6102c562d33bfff6eaedca
Suresh <154883019+Sureshravic@users.noreply.github.com>
2025-10-23 22:22:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import http from "http";
+import http, { IncomingMessage, ServerResponse } from "http";
 
 const port = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from Azure App Service via ACR token demo!\n");
 });
