5cad5a3f1a01194bb892c6c72e6708ff6509b2ea
Suresh <154883019+Sureshravic@users.noreply.github.com>
2025-10-23 23:26:08 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Azure App Service via ACR token demo!\n");
+  res.end("Hello This is the demo of app service from azure acr + github \n");
 });
 
 server.listen(port, () => {
