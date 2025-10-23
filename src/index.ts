141ddea2819334ac11b6bf0966c8fac0aba6927f
Suresh <154883019+Sureshravic@users.noreply.github.com>
2025-10-23 22:04:57 +0530

    Initialize HTTP server in index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Azure App Service via ACR token demo!\n");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
